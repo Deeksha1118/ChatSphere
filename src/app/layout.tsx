6ae3972eed0a966566040e8a6929a96807f96d60
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-02-27 00:40:03 +0530

    Convex Basics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from './../providers/theme-provider';
+import { ConvexClientProvider } from "@/providers/convex-client-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,9 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
           <main className="w-screen h-screen flex">
-	          {children}
+            <ConvexClientProvider>
+	            {children}
+            </ConvexClientProvider>
           </main>
         </ThemeProvider>
       </body>
