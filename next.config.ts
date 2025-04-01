f7ebb2e53e5ae10b3fb66430fef1d07537849ab7
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-04-01 18:44:32 +0530

    sendImage & sendVideo mutations added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {hostname: "little-starling-755.convex.cloud"}
+    ],
+  },
 };
 
 export default nextConfig;
