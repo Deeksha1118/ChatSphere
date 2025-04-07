d53031d1f6cc678a14505a75b6cca885aa2f45ec
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-04-07 16:10:19 +0530

    chatgpt support added

--- src/components/home/chat-bubble.tsx
+++ src/components/home/chat-bubble.tsx
@@ -7,6 +7,7 @@ import ReactPlayer from "react-player";
 import { useState } from "react";
 import { Dialog, DialogContent, DialogDescription } from "../ui/dialog";
 import ChatAvatarActions from "./chat-avatar-actions";
+import { Bot } from "lucide-react";
 
 type ChatBubbleProps = {
 	message: IMessage;
@@ -22,11 +23,13 @@ const ChatBubble = ({me, message, previousMessage}: ChatBubbleProps) => {
 	const time = `${hour}:${minute}`;
 
 	const {selectedConversation} = useConversationStore();
-	const isMember = selectedConversation?.participants.includes(message.sender._id) || false;
+	const isMember = selectedConversation?.participants.includes(message.sender?._id) || false;
 	const isGroup = selectedConversation?.isGroup;
-	const fromMe = message.sender._id === me._id;
-	const bgClass = fromMe ? "bg-green-chat" : "bg-white dark:bg-gray-primary";
+	const fromMe = message.sender?._id === me._id;
+	const fromAI = message.sender?.name === "ChatGPT";
+	const bgClass = fromMe ? "bg-green-chat" : !fromAI ? "bg-white dark:bg-gray-primary" : "bg-blue-500 text-white";
 
+	console.log(message.sender);
 	const [open, setOpen] = useState(false);
 
 	const renderMessageContent = () => {
@@ -47,10 +50,13 @@ const ChatBubble = ({me, message, previousMessage}: ChatBubbleProps) => {
 			<>
 				<DateIndicator message={message} previousMessage={previousMessage} />
 				<div className="flex gap-1 w-2/3">
-				<ChatBubbleAvatar isGroup={isGroup} isMember={isMember} message={message} />
+				<ChatBubbleAvatar isGroup={isGroup} isMember={isMember} message={message} 
+					fromAI={fromAI}
+				/>
 					<div className={`flex flex-col z-20 max-w-fit px-2 pt-1 rounded-md shadow-md relative ${bgClass}`}>
-						<OtherMessageIndicator />
-						{isGroup && <ChatAvatarActions 
+						{!fromAI && <OtherMessageIndicator />}
+						{fromAI && <Bot size={16} className="absolute bottom-[2px] left-2" />}
+						{<ChatAvatarActions 
 							message={message}
 							me={me}
 						/>}
--- src/components/home/chat-placeholder.tsx
+++ src/components/home/chat-placeholder.tsx
@@ -5,18 +5,18 @@ import { Button } from "../ui/button";
 const ChatPlaceHolder = () => {
 	return (
 		<div className='w-3/4 bg-gray-secondary flex flex-col items-center justify-center py-10'>
-			<div className='flex flex-col items-center w-full justify-center py-10 gap-4'>
-				<Image src={"/desktop-hero.png"} alt='Hero' width={320} height={188} />
-				<p className='text-3xl font-extralight mt-5 mb-2'>Download WhatsApp for Windows</p>
-				<p className='w-1/2 text-center text-gray-primary text-sm text-muted-foreground'>
-					Make calls, share your screen and get a faster experience when you download the Windows app.
+			<div className='flex flex-col items-center w-full justify-center mt-20'>
+				<Image src={"/logo.png"} alt='Hero' width={350} height={350} className="border-black"/>
+				<p className='text-3xl font-extralight pt-10'>Welcome to ChatSphere!!</p>
+				<p className='w-1/2 text-center text-gray-primary text-sm text-muted-foreground mt-10'>
+					Login, And Stay Connected with your Friends And Family
 				</p>
 
-				<Button className='rounded-full my-5 bg-green-primary hover:bg-green-secondary'>
-					Get from Microsoft Store
+				<Button className='rounded-full mt-10 bg-blue-500 border'>
+					Login
 				</Button>
 			</div>
-			<p className='w-1/2 mt-auto text-center text-gray-primary text-xs text-muted-foreground flex items-center justify-center gap-1'>
+			<p className='w-1/2 mt-auto text-center text-gray-primary text-xs text-muted-foreground flex items-center justify-center gap-1 py-10'>
 				<Lock size={10} /> Your personal messages are end-to-end encrypted
 			</p>
 		</div>
