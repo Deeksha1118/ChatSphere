a049fa3d6193b0bbaf55d4335b7009662b74a81d
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-04-08 13:41:06 +0530

    Fix: removed ny from useComponentVisible hook

--- src/hooks/useComponentVisible.ts
+++ src/hooks/useComponentVisible.ts
@@ -1,27 +1,27 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, RefObject, Dispatch, SetStateAction } from "react";
 
 interface ComponentVisibleHook {
-	ref: React.RefObject<any>;
-	isComponentVisible: boolean;
-	setIsComponentVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  ref: RefObject<HTMLDivElement>;
+  isComponentVisible: boolean;
+  setIsComponentVisible: Dispatch<SetStateAction<boolean>>;
 }
 
 export default function useComponentVisible(initialIsVisible: boolean): ComponentVisibleHook {
-	const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible);
-	const ref = useRef<any>(null);
+  const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible);
+  const ref = useRef<HTMLDivElement>(null);
 
-	const handleClickOutside = (event: MouseEvent) => {
-		if (ref.current && !ref.current.contains(event.target as Node)) {
-			setIsComponentVisible(false);
-		}
-	};
+  const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsComponentVisible(false);
+    }
+  };
 
-	useEffect(() => {
-		document.addEventListener("click", handleClickOutside, true);
-		return () => {
-			document.removeEventListener("click", handleClickOutside, true);
-		};
-	}, []);
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
 
-	return { ref, isComponentVisible, setIsComponentVisible };
+  return { ref, isComponentVisible, setIsComponentVisible };
 }
